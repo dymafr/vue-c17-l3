bab3b6fd953e96821f6ad7030605eb659cbeabcb
dymafr <erwan1309@hotmail.com>
2022-03-30 11:49:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-yskywq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-yskywq)
---
+++ src/shared/services/product.service.ts
@@ -0,0 +1,18 @@
+import type { FiltersInterface, ProductInterface } from '../../interfaces';
+
+export async function fetchProducts(
+  filter: FiltersInterface,
+  page: number
+): Promise<ProductInterface[] | ProductInterface> {
+  const query = new URLSearchParams();
+  if (filter.category !== 'all') {
+    query.append('category', filter.category);
+  }
+  query.append('limit', '20');
+  query.append('price', `$lte:${filter.priceRange[1]}`);
+  query.append('price', `$gte:${filter.priceRange[0]}`);
+  const products = await (
+    await fetch(`https://restapi.fr/api/vueprojectproducts?${query}`)
+  ).json();
+  return products;
+}
